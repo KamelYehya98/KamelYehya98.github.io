9f04168c3d37945320c24f020c7d068a2bda935c
Kamel Yehya <kamel.yehya04@gmail.com>
2021-05-18 15:09:54 +0300

    update meta

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Free Online Card Game">
+    <meta name="keywords" content="Card, Game, Monke, Online, Card Game, Online Card Game, Monke Card Game">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/signin.css">
