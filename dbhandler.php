09921a95e410cc69ba87f89a913cf1434fd885e1
Kamel Yehya <kamel.yehya04@gmail.com>
2021-05-18 13:54:18 +0300

    Update dbhandler.php

--- dbhandler.php
+++ dbhandler.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "monkey";
+$servername = "us-cdbr-east-03.cleardb.com";
+$username = "b0d4a32ee82d23";
+$password = "e056e410";
+$dbname = "heroku_609325a5102c7ea";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 if(!$conn)
